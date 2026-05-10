4c618981db680b97167f2fae0ce0f95908ec8109
Ravi Kumar Kashyap <ravikumarkashyap@Ravis-MacBook-Air.local>
2026-05-10 18:38:31 +0530

    working fix

--- server.js
+++ server.js
@@ -48,4 +48,8 @@ app.post("/chat", async (req, res) => {
     }
 });
 
-module.exports = app;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
